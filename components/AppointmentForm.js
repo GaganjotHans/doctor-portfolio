5f58d8259a9b688d96403ba0185b224836aeb0f8
Gaganjot <gaganjothans777@gmail.com>
2024-05-26 14:34:02 -0400

    Book AppoinemtmentForm created

---
+++ components/AppointmentForm.js
@@ -0,0 +1,160 @@
+import React, { useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+
+const AppointmentForm = () => {
+  const [captchaVal, setCaptchaVal] = useState("");
+  return (
+    <section className="text-gray-600 body-font relative">
+      <div className="container py-16 mx-auto">
+        <div className="lg:w-3/5 md:w-full bg-gray-50 p-12 flex flex-col md:mr-auto w-full md:mt-0 border-2 ">
+          <div className="relative mb-4">
+            <label
+              htmlFor="firstName"
+              className="leading-7 text-md text-gray-900 font-sans"
+            >
+              First Name
+            </label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-800 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              spellCheck="false"
+              data-ms-editor="true"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="lastName"
+              className="leading-7 text-md text-gray-900 font-sans"
+            >
+              Last Name
+            </label>
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-800 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              spellCheck="false"
+              data-ms-editor="true"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="email"
+              className="leading-7 text-md text-gray-900 font-sans"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-800 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="phone"
+              className="leading-7 text-md text-gray-900 font-sans"
+            >
+              Phone
+            </label>
+            <input
+              type="number"
+              id="phone"
+              name="phone"
+              className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-800 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="location"
+              className="leading-7 text-md text-gray-900 font-sans"
+            >
+              Select Nearest Location
+            </label>
+            <select
+              id="location"
+              name="location"
+              className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-800 text-base outline-none text-gray-700 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            >
+              <option value="location1">
+                Hakim Optical (222 Glendale Ave, St. Catharines, ON L2T 2K5)
+              </option>
+              <option value="location2">Location 2</option>
+              <option value="location3">Location 3</option>
+              <option value="location4">Location 4</option>
+              <option value="location5">Location 5</option>
+            </select>
+          </div>
+
+          <div className="relative mb-4">
+            <label className="leading-7 text-md text-gray-900 font-sans">
+              Patient Type
+            </label>
+            <div className="flex items-center mt-2">
+              <input
+                type="radio"
+                id="new-patient"
+                name="patient-type"
+                value="new"
+                className="mr-2 "
+              />
+              <label
+                htmlFor="new-patient"
+                className="text-md text-gray-900 font-sans mr-4"
+              >
+                New Patient
+              </label>
+              <input
+                type="radio"
+                id="existing-patient"
+                name="patient-type"
+                value="existing"
+                className="mr-2"
+              />
+              <label
+                htmlFor="existing-patient"
+                className="text-md text-gray-900 font-sans"
+              >
+                Existing Patient
+              </label>
+            </div>
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="message"
+              id="label-message"
+              className="leading-7 text-md text-gray-900 font-sans"
+            >
+              Ask a question or leave us a specific request
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              rows="4"
+              className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-800 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            ></textarea>
+          </div>
+          {/* <button className="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">
+            Button
+          </button> */}
+          <ReCAPTCHA
+            className="my-2"
+            sitekey="6Lf0Q-YpAAAAANFtyYKB1KWbKv1lZLaecMU_-J_a"
+            onChange={(val) => setCaptchaVal(val)}
+          />
+          <button
+            disabled={!captchaVal}
+            className="inline-flex items-center mr-auto bg-blue-900 text-white border-2 border-blue-900 p-2 focus:outline-none  rounded mt-4 md:mt-0 font-thin text-lg hover:bg-transparent hover:text-blue-900"
+          >
+            Submit Booking Request
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AppointmentForm;
--- pages/exam.js
+++ pages/exam.js
@@ -1,12 +1,72 @@
 import React from "react";
-import ContactForm from "../components/ContactForm";
+import AppointmentForm from "../components/AppointmentForm";
+import Footer from "../components/Footer";
 
 const Exam = () => {
   return (
-    <div>
-      exam
-      <ContactForm />
-    </div>
+    <section className="text-gray-600 body-font">
+      <h1 className=" container mx-auto title-font font-medium text-3xl py-5 text-black font-serif mt-10 text-start border-b-2 border-y-gray-300">
+        EYE EXAM
+      </h1>
+
+      <div className=" container mx-auto flex px-16 py-16 md:flex-row flex-col  bg-gray-50">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-8 md:mb-0 items-center text-center">
+          <p className="mb-8 leading-relaxed font-style">
+            We are dedicated to providing exceptional eye care through
+            comprehensive eye exams, advanced technology, and a patient-centered
+            approach. Our experienced professionals use state-of-the-art
+            diagnostic tools to ensure precise evaluations and effective
+            treatments. Regular eye exams are essential for maintaining optimal
+            vision and overall health, as they can detect early signs of eye
+            conditions and systemic health issues. We prioritize your comfort
+            and satisfaction, offering convenient online booking to make
+            scheduling your eye exam quick and easy. Trust us for all your eye
+            care needs and experience the highest standard of care for your
+            vision. Book your eye exam online today!
+          </p>
+          <ul className="font-style py-5 list-disc list-outside">
+            <li className="my-2">
+              <span className="text-black"> Visual Acuity Test: </span>Assessing
+              how well you see at various distances.
+            </li>
+            <li className="my-2">
+              <span className="text-black"> Refraction Assessment: </span>{" "}
+              Determining the correct prescription for glasses or contact
+              lenses.
+            </li>
+            <li className="my-2">
+              <span className="text-black">Eye Health Evaluation: </span> Using
+              advanced diagnostic tools to examine the internal and external
+              structures of your eyes.
+            </li>
+            <li className="my-2">
+              <span className="text-black">Glaucoma Test: </span> Measuring the
+              pressure inside your eyes to check for glaucoma.
+            </li>
+            <li className="my-2">
+              <span className="text-black"> Macular and Retinal Exam: </span>{" "}
+              Screening for macular degeneration and other retinal conditions.
+            </li>
+          </ul>
+          {/* <button className="inline-flex items-center bg-blue-900 text-white border-2 border-blue-900 p-2 focus:outline-none px-4 rounded mt-4 md:mt-0 font-thin text-lg hover:bg-transparent hover:text-blue-900">
+            Book Your Eye Test Now
+          </button> */}
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img //eslint-disable-line
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="/EyeExamRoom.jpeg"
+          />
+        </div>
+      </div>
+      <h1 className=" container mx-auto title-font font-medium text-3xl py-5 text-black font-serif mt-10 text-start border-b-2 border-y-gray-300">
+        BOOK YOUR EYE EXAM BELOW
+      </h1>
+
+      <AppointmentForm />
+      <Footer />
+    </section>
   );
 };
 
